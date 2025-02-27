a2ac417e653cb9ec3a8558001d5ed7a067000b36
MichaelWHamilton <mwhamilton@ualr.edu>
2025-02-26 20:22:41 -0600

    Grunt part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,9 @@ module.exports = function (grunt) {
     require('time-grunt')(grunt);
 
     //Auto load requirede grunt tasks
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
 
     const sass = require('sass');
     //Define the configurationf or all the tasks
@@ -17,6 +19,125 @@ module.exports = function (grunt) {
                     'css/styles.css' : 'css.styles/scss'
                 }
             },
+        },
+        copy:{
+            html: {
+                files: [
+                {
+                    //for html
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [
+                {
+                    //for font-awesome
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src:['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            },
+            clean: {
+                build: {
+                    src: ['dist/']
+                }
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cdw: './',
+                    src: ['img/*.{png,jpg,gif}'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+        clean:{
+            build:['dist']
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html', 'aboutus.html', 'index.html'] 
+            },
+            options:{
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: { separator: ';'},
+            dist:{
+                files: {
+                    'dist/js/jquery.js': ['node_modules/jquery/dist/jquery.slim.min.js'],
+                    'dist/js/popper.js': ['node_modules/popper.js/dist/umd/popper.min.js'],
+                    'dist/js/bootstrap.js': ['node_modules/bootstrap/dist/js/bootstrap.min.js'],
+                    'dist/js/scripts.js': ['js/scripts.js'] 
+                }
+            },
+            
+        },
+        uglify: {
+            dist: {}
+        },
+        csmin: {
+            dist: {}
+        },
+        filerev: {
+            options:{
+                encoding: 'utf8',
+                algorithm: 'md5',
+                lenght: 20
+            },
+            relase: {
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css'
+                    ]
+                }]
+            }
+        },
+        usemin:{
+            html: ['dist/contactus.html','dist/aboutus.html','dist/index.html'],
+            options:{
+                assetsDir: ['dist', 'dist/css', 'dist/js']
+            }
+        },
+        htmlmin:{
+            dist:{
+                options:{
+                    collapseWhitespace: true
+                },
+                files:{
+                    'dist/index.html':'dist/index.html',
+                    'destination':'source',
+                    'dist/contactus.html':'dist/contactus.html',
+                    'dist/aboutus.html':'dist/aboutus.html'
+                }
+            }
         },
         watch: {
             files: 'css/*.scss',
@@ -44,4 +165,16 @@ module.exports = function (grunt) {
 
     grunt.registerTask('css', ['sass']);
     grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build',[
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
 };
